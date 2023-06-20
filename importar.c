14258383f40e0d810b77367451e09da3463f0ce3
sebastian019 <127500554+sebastian019@users.noreply.github.com>
2023-06-20 19:53:20 -0400

    Me fue mal en algebra

--- importar.c
+++ importar.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
+#include "estructuras.c"
 
 const char *get_csv_field (char * tmp, int k) {
     int open_mark = 0;
@@ -52,36 +53,37 @@ void importar(Grafo *g,FILE *archivo){
     paciente* p = (paciente*) malloc(sizeof(paciente));
     p->medicos = createList();
     for(i = 0 ; i < 7 ; i++){//Se realizan 7 ciclos para permitir que se realizen las suficientes operaciones(6 valores en el struct)
-        aux = get_csv_field(linea, i); //aux se convierte en la linea de caracteres i-esima para rellenar el valor correspondiente.
-        if(i == 0){
+      aux = get_csv_field(linea, i); //aux se convierte en la linea de caracteres i-esima para rellenar el valor correspondiente.
+      if(i == 0){
         strcpy(p->nombre, aux);
-        }
-        if(i == 1){
+      }
+      if(i == 1){
         strcpy(p->apellido, aux);
-        }
-        if(i == 2){ 
+      }
+      if(i == 2){ 
         edadInt= atoi(aux);
         p->edad=edadInt;
-        }
-        if(i == 3){
+      }
+      if(i == 3){
         strcpy(p->telefono, aux);
-        }
-        if(i== 4){
+      }
+      if(i== 4){
         strcpy(p->direccion, aux);
-        }
-        if(i == 5){
+      }
+      if(i == 5){
         strcpy(p->numeroSocial, aux);
         Registrar(p, l);
-        } 
-        if(i == 6){
+      } 
+      if(i == 6){
         asignarMedico(l, p->nombre, p->apellido, aux );
-        }
       }
     }
+  }
   fclose(archivo);
 }
 
 void importarArchivos(){
+  Grafo *grafo = createGrafo();
   FILE *archivo = fopen('sucesos.csv',"rt");
   if (archivo == NULL) {
   printf("* Error al abrir el archivo.\n");
